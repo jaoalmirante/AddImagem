48e84e0694e9e66a007ec0ea4c6e49bb5a58f71a
jaoalmirante <joaoalmirante06@gmail.com>
2023-05-18 11:18:32 -0300

    Deixando apresentável

--- main.js
+++ main.js
@@ -1,36 +1,31 @@
-function cadastrar(){
+function cadastrar() {
     //capturar dados o input
-    let titulo = document.getElementById('titulo').value
-    let link = document.getElementById('link').value
-    let descricao = document.getElementById('descricao').value
-
+    let titulo = document.getElementById('titulo').value;
+    let link = document.getElementById('link').value;
+    let descricao = document.getElementById('descricao').value;
 
     //criar elementos para serem adicionados
-    let div = document.createElement('div')
-    let h1 = document.createElement('h1')
-    let img = document.createElement('img')
-    let p = document.createElement('p')
-
+    let div = document.createElement('div');
+    let h1 = document.createElement('h1');
+    let img = document.createElement('img');
+    let p = document.createElement('p');
 
     //capturar dados do tag com id container
-    let container = document.getElementById('container')
+    let container = document.getElementById('container');
 
+    //adicionando classes para os elementos criados
+    div.classList.add('foto');
+    h1.classList.add('agaUm');
+    img.classList.add('imagemLink');
+    p.classList.add('pe');
 
-    //adicionando id's para os elementos criados
-
-
-    div.id='foto'
-    h1.innerHTML = titulo
-    h1.id='agaUm'
-    img.src = link
-    img.id='imagemLink'
-    p.innerHTML = descricao
-    p.id='pe'
+    h1.innerHTML = titulo;
+    img.src = link;
+    p.innerHTML = descricao;
 
     //adiciona os elementos no html
-    div.append(h1)
-    div.append(img)
-    div.append(p)
-    container.append(div)
-
+    div.append(h1);
+    div.append(img);
+    div.append(p);
+    container.append(div);
 }
